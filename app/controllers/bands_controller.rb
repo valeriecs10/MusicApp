423526d0495700fc410ac03a962efd5c168f1a44
Valerie Campbell <valeriecs10@gmail.com>
2021-02-04 15:25:30 -0800

    added band model, routes, controller actions, and views

---
+++ app/controllers/bands_controller.rb
@@ -0,0 +1,54 @@
+class BandsController < ApplicationController
+  def index
+    @bands = Band.all
+    render :index
+  end
+
+  def show
+    @band = Band.find_by_id(params[:id])
+  end 
+
+  def new
+    @band = Band.new
+    render :new
+  end
+
+  def create
+    @band = Band.new(band_params)
+    if @band.save
+      redirect_to bands_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @band = Band.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @band = Band.find_by(id: params[:id])
+
+    if @band.update_attributes(band_params)
+      redirect_to band_url(@band)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @band = Band.find_by_id(params[:id])
+    if @band.destroy
+      redirect_to bands_url
+    else
+      redirect_to band_url(@band)
+    end
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:name)
+  end 
+end
