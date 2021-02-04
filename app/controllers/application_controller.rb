a77e22986174e54d2486757933244deff508b1da
Valerie Campbell <valeriecs10@gmail.com>
2021-02-04 13:18:57 -0800

    added current_user, login_user!, and logged_in? methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    user = User.find_by(session_token: session[:session_token])
+    return nil unless user
+    user
+  end
+
+  def login_user!(user)
+    token = user.reset_session_token!
+    session[:session_token] = token
+  end
+
+  def logged_in?
+    return true if session[:session_token]
+    false
+  end
 end
