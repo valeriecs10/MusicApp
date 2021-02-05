dfe1e035c175bf5f4a4dfc91b88aa7a0fbec938f
Valerie Campbell <valeriecs10@gmail.com>
2021-02-04 16:12:26 -0800

    added albums table

---
+++ db/migrate/20210204232430_create_album.rb
@@ -0,0 +1,13 @@
+class CreateAlbum < ActiveRecord::Migration[5.2]
+  def change
+    create_table :albums do |t|
+      t.string :title, null: false
+      t.integer :year, null: false
+      t.boolean :live, null: false, default: false
+      t.integer :band_id, null: false
+      t.timestamps
+    end
+    add_index :albums, :title
+    add_index :albums, :band_id
+  end
+end
