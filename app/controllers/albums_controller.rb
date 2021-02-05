54de0c19ec989c0ac8ba96884cc7f4468a33cd83
Valerie Campbell <valeriecs10@gmail.com>
2021-02-05 15:55:07 -0800

    added create, show, edit, update, destroy, album_params actions

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,49 @@
 class AlbumsController < ApplicationController
   def new
-    @band_id = params[:band_id].to_i
+    @album = Album.new
     render :new
+  end
+
+  def create
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to album_url(@album)
+    else 
+      redirect_to new_band_album_url(@album.band_id), info: "Album could not be saved."
+    end
+  end
+
+  def show
+    @album = Album.find_by_id(params[:id])
+    render :show
+  end
+
+  def edit
+    @album = Album.find_by_id(params[:id])
+    render :edit
+  end
+
+  def update
+    @album = Album.find_by_id(params[:id])
+    if @album.update_attributes(album_params)
+      redirect_to album_url(@album)
+    else
+      redirect_to edit_album_url(@album), info: "Album could not be updated."
+    end
+  end
+
+  def destroy
+    @album = Album.find_by_id(params[:id])
+    if @album.destroy
+      redirect_to bands_url
+    else
+      redirect_to album_url(@album), info: "Album could not be deleted."
+    end
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(:title, :year, :live, :band_id)
   end
 end
