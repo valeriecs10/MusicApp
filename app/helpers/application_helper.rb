310fac6dbc2f437f72f0fde42a8a056bf344d43f
Valerie Campbell <valeriecs10@gmail.com>
2021-02-23 16:30:30 -0800

    added auth_token, patch_method, render_flash_messagse, and ugly_lyrics methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,30 @@
 module ApplicationHelper
+  include ERB::Util
+
   def auth_token
-    '<input type="hidden" name="authenticity_token" value="#{form_authenticity_token}">'.html_safe
+    "<input type=\"hidden\" name=\"authenticity_token\" value=\"#{form_authenticity_token}\">".html_safe
+  end
+
+  def patch_method
+    "<input type=\"hidden\" name=\"_method\" value=\"patch\">".html_safe
+  end
+
+  def render_flash_messages
+    html = ""
+    flash.each do |type, msgs|
+      msgs.each do |msg|
+        html += "<div class=\"#{type}\">#{msg}.</div>"
+      end
+    end
+    html.html_safe
+  end
+
+  def ugly_lyrics(lyrics)
+    formatted_lyrics = ""
+    lyrics.lines.each do |line|
+      formatted_lyrics << "&#9835; #{h(line)}"
+    end
+
+    "<pre>#{formatted_lyrics}</pre>".html_safe
   end
 end
