c9c83e1c0e2937a0fbed1a838890290a958bde2a
Valerie Campbell <valeriecs10@gmail.com>
2021-02-23 16:05:37 -0800

    added flash messages to update action

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,7 @@
 class AlbumsController < ApplicationController
   def new
     @album = Album.new
+    @band_id = params[:band_id].to_i
     render :new
   end
 
@@ -9,6 +10,7 @@ class AlbumsController < ApplicationController
     if @album.save
       redirect_to album_url(@album)
     else 
+      log_flash_messages
       redirect_to new_band_album_url(@album.band_id), info: "Album could not be saved."
     end
   end
@@ -28,20 +30,28 @@ class AlbumsController < ApplicationController
     if @album.update_attributes(album_params)
       redirect_to album_url(@album)
     else
+      log_flash_messages
       redirect_to edit_album_url(@album), info: "Album could not be updated."
     end
   end
 
   def destroy
     @album = Album.find_by_id(params[:id])
+    band_id = @album.band_id
     if @album.destroy
-      redirect_to bands_url
+      redirect_to band_url(band_id)
     else
       redirect_to album_url(@album), info: "Album could not be deleted."
     end
   end
 
   private
+
+  def log_flash_messages
+    @album.errors.full_messages.each do |msg|
+      flash_message(:alert, msg)
+    end
+  end
 
   def album_params
     params.require(:album).permit(:title, :year, :live, :band_id)
