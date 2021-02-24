adb1f3e0f064f7063231feaba6e14aa439fa638f
Valerie Campbell <valeriecs10@gmail.com>
2021-02-23 16:48:25 -0800

    added nested resources for album tracks and track notes, and added users#activate route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'sessions#new'
+  root 'bands#index'
   resource :session, only: [:create, :destroy, :new]
+  get "/users/activate", to: "users#activate"
   resources :users, only: [:create, :new, :show]
   resources :bands do 
     resources :albums, only: :new
   end
-  resources :albums, only: [:show, :create, :edit, :update, :destroy]
+  resources :albums, only: [:show, :create, :edit, :update, :destroy] do
+    resources :tracks, only: :new
+  end
+  resources :tracks, only: [:create, :edit, :show, :update, :destroy] do
+    resources :notes, only: :new
+  end
+  resources :notes, only: [:create, :show, :destroy]
 end
