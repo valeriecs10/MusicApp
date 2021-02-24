ed5c6cf31695c3937d6cd064a3fb126f224c6719
Valerie Campbell <valeriecs10@gmail.com>
2021-02-23 16:36:42 -0800

    added alias method generate_activation_token, ensure_activation_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ApplicationRecord
   validates :email, :session_token, presence: true, uniqueness: true
-  validates :password_digest, presence: true
-  before_validation :ensure_session_token
+  validates :password, length: { minimum: 6 }, on: :create
+  before_validation :ensure_session_token, :ensure_activation_token
+
+  has_many :notes
+  attr_reader :password
   
   def self.generate_session_token
     SecureRandom::urlsafe_base64
+  end
+
+  class <<self
+    alias_method :generate_activation_token, :generate_session_token
   end
 
   def self.find_by_credentials(email, password)
@@ -15,8 +22,7 @@ class User < ApplicationRecord
   end
 
   def reset_session_token!
-    self.session_token = User.generate_session_token
-    self.save!
+    self.update_attributes(session_token: User.generate_session_token)
     self.session_token
   end
 
@@ -24,9 +30,14 @@ class User < ApplicationRecord
     self.session_token ||= User.generate_session_token
   end
 
+  def ensure_activation_token
+    self.activation_token ||= User.generate_activation_token
+  end
+
   # private
 
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
